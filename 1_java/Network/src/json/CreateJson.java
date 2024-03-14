b96d50be3c1b06198a962637535ea37ffdbe1b28
atimaby28 <atimaby28@gmail.com>
2024-03-14 16:42:48 +0900

    Update : Json

--- 1_java/Network/src/json/CreateJson.java
+++ 1_java/Network/src/json/CreateJson.java
@@ -12,28 +12,28 @@ public class CreateJson {
     public static void main(String[] args) throws IOException {
         JSONObject root = new JSONObject();
 
-        root.put("id","atimaby28");
-        root.put("name","홍길동");
-        root.put("age",31);
-        root.put("student",false);
+        root.put("id", "atimaby28");
+        root.put("name", "홍길동");
+        root.put("age", 31);
+        root.put("student", false);
 
         JSONObject tel = new JSONObject();
-        tel.put("home","02-123-1234");
-        tel.put("mobile","010-123-1234");
-        root.put("tel",tel);
+        tel.put("home", "02-123-1234");
+        tel.put("mobile", "010-123-1234");
+        root.put("tel", tel);
 
         JSONArray skill = new JSONArray();
         skill.put("java");
         skill.put("python");
-        skill.put("c++");
-        root.put("skill",skill);
+        skill.put("AI");
+        root.put("skill", skill);
 
         String json = root.toString();
 
         System.out.println(json);
 
         Writer writer = new FileWriter("./member.json", Charset.forName("UTF-8"));
-        
+
         writer.write(json);
         writer.flush();
         writer.close();
